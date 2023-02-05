ba6bbcae440d2f9f8b90479ca1e375d8ccfb7236
Hrishiraj Sawan <hrishiraj102@gmail.com>
2023-02-06 01:38:39 +0530

    3rd Commit

--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Grid, Button, Box, Typography } from "@mui/material";
 
-function ProjectComponent(){
-    return(
+function ProjectComponent() {
+    return (
         <div>
-            
+
         </div>
     )
 }
